49360bcf6ea841eed7fd5ff87c3a1bed51624298
sc <strong.cod3r@gmail.com>
2019-09-23 08:19:31 +0200

    Start

---
+++ MyServiceWindows/MyServiceWindows/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServiceWindows
+{
+	static class Program
+	{
+		/// <summary>
+		/// Punto de entrada principal para la aplicación.
+		/// </summary>
+		static void Main()
+		{
+#if !DEBUG
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[]
+			{
+				new Service1()
+			};
+			ServiceBase.Run(ServicesToRun);
+		}
+#else
+			new Service1().OnDebug();
+#endif
+		}
+	}
+}
---
+++ MyServiceWindows/MyServiceWindows/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServiceWindows
+{
+	public partial class Service1 : ServiceBase
+	{
+		public Service1()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			Console.WriteLine("Hora: " + DateTime.Now.TimeOfDay);
+			System.Threading.Thread.Sleep(5000);
+		}
+
+		protected override void OnStop()
+		{
+
+		}
+
+		public void OnDebug()
+		{
+			do
+			{
+				this.OnStart(null);
+			} while (true);
+		}
+	}
+}
